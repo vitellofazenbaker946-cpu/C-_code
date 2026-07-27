be8aabe4cc6e5defe039087ecc98e6fca887ff7d
hermit-zhajianyi <3553229391@qq.com>
2026-07-27 08:57:32 +0800

    update cpp learning

---
+++ Vector/Vector/text.cpp
@@ -0,0 +1,47 @@
+#include"vector.h"
+//void vectortext01d()
+//{
+//	vector<int> v1;
+//	vector<int> v2(10, 1);
+//	vector<int> v3(++v2.begin(), --v2.end());
+//
+//	for (int i = 0; i < v2.size(); ++i)
+//	{
+//		cout << v2[i] << ' ';
+//	}
+//	cout << endl;
+//
+//	vector<int>::iterator it = v2.begin();
+//	while (it != v2.end())
+//	{
+//		cout << *it << " ";
+//		++it;
+//	}
+//	cout << endl;
+//
+//	for (auto e : v2)
+//	{
+//		cout << e << " ";
+//	}
+//	cout << endl;
+//
+//	vector<int> v(5, 1);
+//	vector<vector<int>> vv(10, v);
+//
+//}
+//
+//
+//int main()
+//{
+//	vectortext01d();
+//	return 0;
+//}
+
+int main()
+{
+	//Jianyi::text01();
+	//Jianyi::text02();
+	Jianyi::text05();
+
+	return 0;
+}
